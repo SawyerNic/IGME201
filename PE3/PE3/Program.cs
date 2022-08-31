3c9a56cf9c4e5ab7f1df0c8e0c2fb0bbb41540c4
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2022-08-31 14:08:20 -0400

    changes

---
+++ PE3/PE3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string Max_Size = "supercalifragilisticexpialidocious";
+
+Console.WriteLine(Max_Size);
