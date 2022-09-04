32fcd371642b1abe5b6d838d94b5f7904e77e07f
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2022-09-04 17:46:52 -0400

    ADDED

---
+++ PE4/PE4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project4
+{
+    //Class: program
+    //Author: Sawyer Nicastro
+    class program
+    {
+
+        static void Main(String[] args)
+        {
+            //var1 and var2 will obtain values given by user
+            int var1 = 0;
+            int var2 = 0;
+
+            //prompt changes if user doesnt get it on first try
+            string prompt = "Enter two numbers";
+
+            //while loop wont terminate unless there is one number over ten and one number under
+            while ((var1 > 10 && var2 < 10) || (var1 < 10 && var2 > 10))
+            {
+                //ask user to enter two numbers
+                Console.Write(prompt);
+
+                //parse input to int
+                var1 = int.Parse(Console.ReadLine());
+                var2 = int.Parse(Console.ReadLine());
+
+                //change prompt in case user guesses incorrect
+                prompt = "Enter two different numbers";
+            }
+
+
+        }
+    }
+}
