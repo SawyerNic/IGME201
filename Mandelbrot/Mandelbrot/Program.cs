bb3e72549c2a1f6b2783a5493168733c49deba9c
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2022-09-05 22:35:43 -0400

    mandelbrot

--- Mandelbrot/Mandelbrot/Program.cs
+++ Mandelbrot/Mandelbrot/Program.cs
@@ -24,9 +24,59 @@ namespace Mandelbrot
             double realCoord, imagCoord;
             double realTemp, imagTemp, realTemp2, arg;
             int iterations;
-            for (imagCoord = 1.2; imagCoord >= -1.2; imagCoord -= 0.05)
+
+            //user input has to enter values between 1.2 and -1.2
+            double highLim = 2, lowLim = -2;
+            double newInc;
+            string prompt = "Enter new upper limit between 1.2 and -1.2";
+
+            //loop doesnt exit unless the highLim is within bounds
+            while (!(highLim > -1.2 && highLim < 1.2))
             {
-                for (realCoord = -0.6; realCoord <= 1.77; realCoord += 0.03)
+                Console.WriteLine(prompt);
+                highLim = double.Parse(Console.ReadLine());
+                prompt = "Enter WITHIN limit";
+            }
+
+            prompt = "Enter new lower limit between 1.2 and -1.2 that is lower than your previous entry";
+            //loop doesnt exit unless the lowLim is within bounds
+            while (!(lowLim > -1.2 && lowLim < 1.2 && lowLim < highLim))
+            {
+                Console.WriteLine(prompt);
+                lowLim = double.Parse(Console.ReadLine());
+                prompt = "Enter valid input";
+            }
+
+            //calculate how much the incriment should be modified by
+            double frac = (highLim - lowLim) / 2.4;
+
+            double topLim = 2;
+            double botLim = -1;
+            prompt = "Enter new top limit between -.6 and 1.77";
+
+            //loop doesnt exit unless the highLim is within bounds
+            while (!(topLim > -.6 && topLim < 1.77))
+            {
+                Console.WriteLine(prompt);
+                topLim = double.Parse(Console.ReadLine());
+                prompt = "Enter WITHIN limit";
+            }
+
+            prompt = "Enter new top limit between -.6 and 1.77";
+
+            //loop doesnt exit unless the lowLim is within bounds
+            while (!(botLim > -.6 && botLim < 1.77 && botLim < topLim))
+            {
+                Console.WriteLine(prompt);
+                botLim = double.Parse(Console.ReadLine());
+                prompt = "Enter valid input";
+            }
+
+            double frac1 = (topLim - botLim) / 2.4;
+
+            for (imagCoord = highLim; imagCoord >= lowLim; imagCoord -= 0.05*frac)
+            {
+                for (realCoord = botLim; realCoord <= topLim; realCoord += 0.03*frac1)
                 {
                     iterations = 0;
                     realTemp = realCoord;
