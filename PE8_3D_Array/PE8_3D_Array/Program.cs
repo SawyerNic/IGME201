7187dc28c5e182337bf7c7fbfd19020a95cb0216
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2022-09-14 19:31:34 -0400

    pe8

---
+++ PE8_3D_Array/PE8_3D_Array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace numba
+{
+    class test
+    {
+        static void Main()
+        {
+
+            //we have to compute the value of z in z = 3y^2 + 2x - 1
+            double x;
+            double y;
+            double z;
+            double[,,] numArr = new double[21, 31, 3];
+            int nx = 0;
+            int ny = 0;
+
+
+            for (x = -1; x <= 1; x += .1)
+            {
+                x = Math.Round(x, 1);
+
+                for (y = 1; y <= 4; y += .1)
+                {
+                    y = Math.Round(y, 1);
+
+                    z = 3 * Math.Pow(y, 2) + 2 * x - 1;
+                    z = Math.Round(z, 3);
+                    //Console.WriteLine(x + " " + y + " " + z);
+
+                    numArr[nx, ny, 0] = x;
+                    numArr[nx, ny, 1] = y;
+                    numArr[nx, ny, 2] = z;
+
+                    ny++;
+                }
+
+                nx++;
+                ny = 0;
+            }
+        }
+    }
+}
