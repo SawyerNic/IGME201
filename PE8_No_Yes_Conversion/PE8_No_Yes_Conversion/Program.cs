4b9fae2051c4a328a2cb76b5b1257cd6c1545981
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2022-09-14 21:36:40 -0400

    PE8

---
+++ PE8_No_Yes_Conversion/PE8_No_Yes_Conversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Internal;
+
+namespace reverseString
+{
+    class test
+    {
+        static void Main()
+        {
+            //Take string from user
+            string userInput = Console.ReadLine();
+            string lowerCase = userInput.ToLower();
+            string output = "";
+
+
+            string[] split = lowerCase.Split("yes");
+
+
+            foreach (string i in split)
+            {
+                output += i + "no";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
