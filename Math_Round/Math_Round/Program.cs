850dc3f64753fbd7bc063ec8625578a325b4eb91
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2022-09-26 19:06:14 -0400

    round delegate

--- Math_Round/Math_Round/Program.cs
+++ Math_Round/Math_Round/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace MathPowManual
+{
+    class Program
+    {
+
+        delegate double MathRound(double x);
+
+        public static void Main()
+        {
+            MathRound round = new MathRound(Math.Round);
+            double rounded = round(6.9);
+            Console.WriteLine(rounded);
+            Console.ReadLine();
+
+        }
+
+    }
+}
 
