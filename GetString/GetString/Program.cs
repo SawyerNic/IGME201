76e32931eb38d075cf9f020714c8f079c707be08
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2022-10-03 23:43:04 -0400

    courselib

--- GetString/GetString/Program.cs
+++ GetString/GetString/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Internal;
 
+namespace Test
+{
+    class Program
+    {
+
+        public abstract class MyClass
+        {
+            private String myString;
+
+            public String MyString
+            {
+                set
+                {
+                    myString = Console.ReadLine();
+                }
+
+            }
+
+
+            public virtual String GetString()
+            {
+
+                return MyString;
+            }
+
+        }
+
+
+        public class MyDerivedClass : MyClass
+        {
+
+            public virtual String GetString()
+            {
+
+                // return MyString + " appended from derived class";
+
+            }
+        }
+    }
+}
